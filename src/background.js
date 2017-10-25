197a762cd206b976b7b134ce0f8a30a87c61dd14
malkailany <mohammed_alkailany@hotmail.co.uk>
2017-10-25 15:27:53 +0100

    added a select box
    load already saved items

--- src/background.js
+++ src/background.js
@@ -1,52 +1,13 @@
-/*
-chrome.extension.onMessage.addListener(function(request, sender, sendResponse)
-{
-
-    if(request.action)
-    {
-        chrome.windows.getAll({populate:true},function(windows){
-            windows.forEach(function(window){
-                window.tabs.forEach(function(tab){
-                    //collect all of the urls here, I will just log them instead
-                    //console.log(tab.url);
-                    allUrl = tab.url;
-
-                        // Get a value saved in a form.
-                        var theValue = allUrl;
-                        // Check that there's some code there.
-                        if (!theValue) {
-                            message('Error: No value specified');
-                            return;
-                        }
-                        // Save it using the Chrome extension storage API.
-                        chrome.storage.sync.set({'value': theValue}, function() {
-                            // Notify that we saved.
-                            console.log('Settings saved');
-                            chrome.storage.sync.get('value', function (obj) {
-                                console.log('value', obj);
-                        });
-
-
-                    chrome.extension.sendMessage({ action: tab.url});
-
-                });
-            });
-        });
-
-        });
-    }
-});
-*/
-chrome.extension.onMessage.addListener(function(request, sender, sendResponse) {
-
+chrome.extension.onMessage.addListener(function (request, sender, sendResponse) {
     if (request.action) {
         var tabarray = new Array();
+
         function getUrl() {
             chrome.windows.getAll({
                 populate: true
-            }, function(windows) {
-                windows.forEach(function(window) {
-                    window.tabs.forEach(function(tab) {
+            }, function (windows) {
+                windows.forEach(function (window) {
+                    window.tabs.forEach(function (tab) {
                         //collect all of the urls here, I will just log them instead
                         tabarray.push(tab.url)
                     });
@@ -60,18 +21,19 @@ chrome.extension.onMessage.addListener(function(request, sender, sendResponse) {
         function saveUrl() {
             console.log("array length: " + tabarray.length)
             var name = request.data
-            var obj= {};
+            var obj = {};
             obj[name] = tabarray;
 
-            chrome.storage.sync.set(obj, function() {
+            chrome.storage.sync.set(obj, function () {
                 // Notify that we saved.
                 console.log('Settings saved as ' + obj[name]);
 
             });
         }
+
         function loadUrl() {
             arrayName = request.data;
-              chrome.storage.sync.get(arrayName, function(items) {
+            chrome.storage.sync.get(arrayName, function (items) {
                 console.log('sync: ' + arrayName);
                 console.log(arrayName, items);
             });
@@ -79,12 +41,11 @@ chrome.extension.onMessage.addListener(function(request, sender, sendResponse) {
 
 
         if (request.action == 'saveme') {
-
-            console.log("You have pressed the save button! Filename is '"+request.data+"'");
+            console.log("You have pressed the save button! Filename is '" + request.data + "'");
             getUrl()
         }
         if (request.action == 'loadme') {
-            console.log("You have pressed the load button! Filename is '"+request.data+"'");
+            console.log("You have pressed the load button! Filename is '" + request.data + "'");
             loadUrl(request.data);
         }
     }
--- src/content.js
+++ src/content.js
@@ -1,48 +1,37 @@
-/*
-document.addEventListener("DOMContentLoaded", function() {
-    document.getElementById("savetabs").addEventListener("click", function () {
 
-        console.log('are we here?');
-
-        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-
-            chrome.tabs.sendMessage(tabs[0].id, {greeting: 'hello'}, function() {
-
-                console.log('were gettings somewhere... but where?');
-                console.log('lets try to get a farewell from background..');
-                chrome.extension.sendMessage({ action: "hellofriend"});
-            });
-        });
-    });
-
-
-});*/
-
-
-
-
-
-    document.addEventListener("DOMContentLoaded", function () {
-        document.getElementById("savetabs").addEventListener("click", function () {
-            var filename = document.getElementById("filename").value;
-            //alert('second ' + filename)
-            chrome.extension.sendMessage({action: "saveme", data: filename});
-        });
-
-
-        document.getElementById("loadtabs").addEventListener("click", function () {
-            var filename = document.getElementById("filename").value;
-            chrome.extension.sendMessage({action: "loadme", data: filename});
-
-        });
-    });
-
-/*
-chrome.extension.sendMessage({ action: "WhatYouWant"});
-
-chrome.extension.onMessage.addListener(function(request, sender, sendResponse){
-    if(request.action){
-
+chrome.storage.sync.get(null, function(items) {
+    var allKeys = Object.keys(items);
+    var select = document.getElementById("loaditem");
+    for(var i = 0; i < allKeys.length; i++) {
+        var opt = allKeys[i]
+        var el = document.createElement("option");
+        el.textContent = opt;
+        el.value = opt;
+        select.appendChild(el);
     }
 
-});*/
+    console.log(allKeys.length)
+   /* var select = document.getElementById("loaditem");
+    for(var i = 0; i < allKeys.length; i++) {
+        var opt = options[i];
+        var el = document.createElement("option");
+        el.textContent = opt;
+        el.value = opt;
+        select.appendChild(el);
+    }​*/
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById("savetabs").addEventListener("click", function () {
+        var filename = document.getElementById("filename").value;
+        //alert('second ' + filename)
+        chrome.extension.sendMessage({action: "saveme", data: filename});
+    });
+    document.getElementById("loadtabs").addEventListener("click", function () {
+        var e = document.getElementById("loaditem");
+        var filename = e.options[e.selectedIndex].text
+        alert(filename);
+        chrome.extension.sendMessage({action: "loadme", data: filename});
+
+    });
+});
